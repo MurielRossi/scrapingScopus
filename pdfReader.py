6e9264f062e30b71b1454d158704579e46f965d9
Muriel <m.rossi67@studenti.unisa.it>
2022-10-03 22:21:59 +0200

    Aggiunto modulo per la lettura dei pdf

--- pdfReader.py
+++ pdfReader.py
@@ -1,11 +1,35 @@
+import glob
+
 import pandas as pd
 import PyPDF2
 
-pdfFileObj = open('pdfTirocinio/A_machine_learning_COVID19_mass_screening_based_on_symptoms_and.pdf', 'rb')
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+array_path = []
 
-pageObj = pdfReader.getPage(i)
-page1 = pageObj.extractText()
-print(page1)
-#df.to_csv("./test.csv")
+array_path = glob.glob("pdfTirocinio/*")
+print(array_path)
+e = 0
+for x in array_path:
+    print(x)
+    pdfFileObj = open(x, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    num_pages = len(pdfReader.pages)
+    print(num_pages)
+    lista = []
+    lista.append({
+        "TITLE": x,
+        "CONTENT": ""
+    })
 
+    i = 0
+    while i < num_pages:
+        pageObj = pdfReader.getPage(i)
+        page1 = pageObj.extractText()
+        print(page1)
+        lista[e]["CONTENT"] + page1
+
+        i = i + 1
+
+    ciao = pd.DataFrame.from_records(lista)
+    print(ciao)
+    ciao.to_csv('\pdfToCSV\p' + x, sep=',')
+    e = e + 1
