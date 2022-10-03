3d5a88360e02ed26c1a7056e954853b7dda74e4e
Muriel Rossi <m.rossi67@studenti.unisa.it>
2022-10-04 01:19:15 +0200

    Modificata la funzione per raccogliere tutto il materiale dei pdf

--- pdfReader.py
+++ pdfReader.py
@@ -8,28 +8,37 @@ array_path = []
 array_path = glob.glob("pdfTirocinio/*")
 print(array_path)
 e = 0
+
 for x in array_path:
-    print(x)
     pdfFileObj = open(x, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     num_pages = len(pdfReader.pages)
     print(num_pages)
-    lista = []
-    lista.append({
-        "TITLE": x,
-        "CONTENT": ""
-    })
+    array = []
+    lista = ""
 
     i = 0
     while i < num_pages:
         pageObj = pdfReader.getPage(i)
         page1 = pageObj.extractText()
-        print(page1)
-        lista[e]["CONTENT"] + page1
+        lista = lista + page1
+        print(lista)
 
         i = i + 1
 
-    ciao = pd.DataFrame.from_records(lista)
-    print(ciao)
-    ciao.to_csv('\pdfToCSV\p' + x, sep=',')
+    array.append(lista)
+
     e = e + 1
+
+ciao = []
+
+i = 0
+for x in array:
+    ciao.append({
+        "NUMBER": str(i),
+        "CONTENT": x
+    })
+
+ciao = pd.DataFrame.from_records(array)
+ciao.to_csv('/home/muriel/PycharmProjects/pythonProject1/pdfToCSV/myPDF.csv')
+
