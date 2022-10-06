824dc5048d50a7c5b8b446987ebea1b57f88d338
Muriel Rossi <m.rossi67@studenti.unisa.it>
2022-10-06 17:01:18 +0200

    Inizio esaminazione dei vari pdf

--- GPT-3_utilizator.py
+++ GPT-3_utilizator.py
@@ -1,15 +1,40 @@
+import json
+
+import jsonlines as jsonlines
 import openai
+import pandas as pd
+import os
 
+jsons = []
+df = pd.read_csv('/home/muriel/PycharmProjects/pythonProject1/pdfToCSV/myPDF.csv')
+
+
+def save_json(x):
+    global jsons
+    jsons.append({"text": x})
+
+
+print(df.columns)
+
+df = df["CONTNENT"].apply(save_json)
+with jsonlines.open('/home/muriel/PycharmProjects/pythonProject1/jsonFIle/myfile.jsonl', 'w') as writer:
+    writer.write_all(jsons)
+
+
+#openai.api_key = os.getenv("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")
 openai.api_key = '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'
+
+start_sequence = "\nA:How IA is used?"
+restart_sequence = "\n\nQ: "
 response = openai.Completion.create(
-    engine="davinci",
-    prompt="Today I'm feeling very happy and",
-    max_tokens=15,
-    echo=True
+    model="text-davinci-002",
+    prompt="ciao uihytrgiuhrtgoiushrg biuehrfiu iuyaryeft uoyawegfr uyaehgf uyaehgf uyweghr jhasyedfb ayuheg wuyeghfr uywehgr oquyhwefr joyuwheg ow3uyjgr oyhwegr ",
+    temperature=0,
+    max_tokens=100,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    stop=["\n"]
 )
 
-openai.Answer.create(
-
-)
-
-print(response.choices[0].text)
+print(response)
